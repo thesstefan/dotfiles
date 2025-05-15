d3ec5425c2efb301cfd9c36de369722124354198
Stefan Stefanache <stefan@stefanache.com>
2025-05-16 00:22:42 +0200

    Add support for KEYWORD(username): matching in folke/todo-comments config

--- nvim/.config/nvim/lua/plugins/todo.lua
+++ nvim/.config/nvim/lua/plugins/todo.lua
@@ -1,4 +1,8 @@
 return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+        search = { pattern = [[\b(KEYWORDS)(\([^\)]*\))?:]] },
+        highlight = { pattern = [[.*<((KEYWORDS)%(\(.{-1,}\))?):]] },
+    },
 }

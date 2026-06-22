78ff98d343bdd879545e48ae4e09a91e98a5a2e9
Stefan Stefanache <stefan@stefanache.com>
2026-06-23 01:34:21 +0200

    Add server_vars to zsh

---
+++ zsh/.zsh/server_vars.zsh
@@ -0,0 +1,7 @@
+export INFRA_ROOT="/srv/infra"
+export STATE_ROOT="/srv/state"
+export DATA_ROOT="/mnt/data"
+export OLLAMA_MODELS="/srv/models"
+export TZ="Europe/Zurich"
+export PUID=1000
+export PGID=1000
